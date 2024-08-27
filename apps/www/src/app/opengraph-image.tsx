df904957a2dded2798c6f4cdd88172d57a788123
Gabriel Fonseca <jg.fonseca@outlook.pt>
2024-08-27 21:23:26 +0100

    og

--- apps/www/src/app/opengraph-image.tsx
+++ apps/www/src/app/opengraph-image.tsx
@@ -18,7 +18,7 @@ export const contentType = 'image/png';
 export default async function Image() {
   // Font
   const interSemiBold = fetch(
-    new URL('./Inter-SemiBold.ttf', import.meta.url)
+    new URL('../fonts/EditorialNew-Bold.woff2', import.meta.url)
   ).then((res) => res.arrayBuffer())
  
   return new ImageResponse(
