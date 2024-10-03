81652643ef0987d6415eabb33083cd45f859a9e1
Gabriel Fonseca <jg.fonseca@outlook.pt>
2024-10-03 09:56:47 +0100

    font size update

--- apps/www/src/components/ui/sonner.tsx
+++ apps/www/src/components/ui/sonner.tsx
@@ -19,8 +19,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className='toaster group'
       toastOptions={{
         classNames: {
-          toast: 'group toast group-[.toaster]:bg-gray-200 dark:group-[.toaster]:bg-gray-1200 group-[.toaster]:text-black dark:group-[.toaster]:text-white group-[.toaster]:border-gray-400 dark:group-[.toaster]:border-gray-1000 group-[.toaster]:shadow-lg',
-          description: 'group-[.toast]:text-black dark:group-[.toast]:text-white',
+          toast: 'group group-[.toaster]:text-sm toast group-[.toaster]:bg-gray-200 dark:group-[.toaster]:bg-gray-1200 group-[.toaster]:text-black dark:group-[.toaster]:text-white group-[.toaster]:border-gray-400 dark:group-[.toaster]:border-gray-1000 group-[.toaster]:shadow-lg',
+          description: 'group-[.toast]:text-sm group-[.toast]:text-black dark:group-[.toast]:text-white',
           actionButton: 'group-[.toast]:bg-white group-[.toast]:text-black dark:group-[.toast]:bg-black dark:group-[.toast]:text-white',
           cancelButton: 'group-[.toast]:bg-white group-[.toast]:text-black dark:group-[.toast]:bg-black dark:group-[.toast]:text-white',
         },
