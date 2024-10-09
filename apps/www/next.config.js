696cbdabe0e203501357591668466f71141135fe
Gabriel Fonseca <jg.fonseca@outlook.pt>
2024-10-09 22:25:16 +0100

    build fix

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -4,9 +4,15 @@
 
 const { withContentlayer } = require('next-contentlayer');
 
+const path = require('path');
+
 // Next config
 const config = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
 };
 
 // Export default config with contentlayer
