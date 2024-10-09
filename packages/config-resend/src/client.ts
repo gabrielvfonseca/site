70e8c8335c174db3cc3d7903d24fed481d37d089
Gabriel Fonseca <jg.fonseca@outlook.pt>
2024-10-09 13:02:22 +0100

    removed non used modules

--- packages/config-resend/src/client.ts
+++ packages/config-resend/src/client.ts
@@ -1,10 +1,5 @@
 import { Resend } from 'resend';
 
-import * as dotenv from 'dotenv';
-
-// Import node-fetch
-dotenv.config();
-
-const resend = new Resend(process.env.RESEND_API_KEY || 're_iXmSVD9x_DQyYxZ4PnqNBx71g5qg1Ga1D');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default resend;
