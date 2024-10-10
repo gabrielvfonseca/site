8f8c39ccc893431ec59691b220671ce9d14eeb0d
Gabriel Fonseca <jg.fonseca@outlook.pt>
2024-10-10 09:06:09 +0100

    modules build update

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -4,16 +4,20 @@
 
 const { withContentlayer } = require('next-contentlayer');
 const { resolve } = require('path');
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 // Next config
 const config = {
   /* config options here */
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': resolve(__dirname, './src'),
       '.contentlayer/generated': resolve(__dirname, '.contentlayer/generated')
     };
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
     return config;
   },
 };
