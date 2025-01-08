f142287d4d43a7d4eda8f7573b4bbbf20714918d
Gabriel Fonseca <jg.fonseca@outlook.pt>
2025-01-08 13:34:01 +0000

    fonts update
    Changed from Inter to Geist

--- packages/design-system/lib/fonts.ts
+++ packages/design-system/lib/fonts.ts
@@ -1,21 +1,10 @@
 import { cn } from '@repo/design-system/lib/utils';
 
-import { Inter, JetBrains_Mono } from 'next/font/google';
-
-const sans = Inter ({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-sans',
-});
-
-const mono = JetBrains_Mono ({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-mono',
-});
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 
 export const fonts = cn(
-  sans.variable,
-  mono.variable,
+  GeistSans.variable,
+  GeistMono.variable,
   'font-sans antialiased'
 );
