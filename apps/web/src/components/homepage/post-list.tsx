4e63c8dda271e5845df1f1b67c78d94aefdca0a1
Gabriel Fonseca <jg.fonseca@outlook.pt>
2025-01-08 13:34:56 +0000

    post list update
    Changed from list structure to card structure of posts

--- apps/web/src/components/homepage/post-list.tsx
+++ apps/web/src/components/homepage/post-list.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 import { formatDate } from '@repo/design-system/lib/utils';
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardTitle,
+} from "@repo/design-system/components/ui/card";
+
+import { Badge } from "@repo/design-system/components/ui/badge";
 
 import { allPosts, type Post } from '@contentlayer';
 
@@ -10,20 +21,28 @@ export const PostList = () => (
             allPosts
                 .sort((a: Post, b: Post) => new Date(b.date).getTime() - new Date(a.date).getTime())
                 .map((post: Post) => (
-                    <a
+                    <Link
                         key={post._id}
-                        rel='noopener noreferrer' 
                         href={post.slug}
-                        className='flex sm:items-center flex-col sm:flex-row gap-0.5 sm:gap-4 group'
+                        className='border-border'
                     >
-                        <span className='line-clamp-2 text-primary group-hover:text-blue-800'>
-                            {post.title}
-                        </span>
-                        <span className='hidden sm:flex flex-1 border-t border-1 border-dashed shrink' />
-                        <span className='font-mono text-[16px] flex-none text-md text-tertiary'>
-                            {formatDate(post.date)}
-                        </span>
-                    </a>
+                        <Card className='relative'>
+                            <CardContent className='w-2/3'>
+                                <CardTitle>
+                                    {post.title}
+                                </CardTitle>
+                                <CardDescription>
+                                    {post.description}
+                                </CardDescription>
+                            </CardContent>
+                            <Badge
+                                variant='outline'
+                                className='absolute top-2 right-2'
+                            >
+                                {formatDate(post.date)}
+                            </Badge>
+                        </Card>
+                    </Link>
             ))
         }
     </ul>
