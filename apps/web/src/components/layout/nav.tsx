594eb8b65b849afecfce762cf579b786ad285865
Gabriel Fonseca <jg.fonseca@outlook.pt>
2025-01-14 18:31:43 +0000

    update nav
    Move a to link

--- apps/web/src/components/layout/nav.tsx
+++ apps/web/src/components/layout/nav.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 const Nav = () => (
     <header className='mb-14 sm:mb-24 flex justify-between items-center tracking-tight w-full'>
         <div className='flex flex-col items-start'>
-            <a   
+            <Link  
                 href='/'
                 className='mb-px inline-block font-medium no-underline hover:text-primary'
             >
                 Gabriel Fonseca
-            </a>
+            </Link>
             <p className='leading-none font-medium text-secondary'>
                 Software Developer
             </p>
