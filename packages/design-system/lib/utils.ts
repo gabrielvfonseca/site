a3dbb21ead1cae792a384b4522313ad66799b97f
Gabriel Fonseca <jg.fonseca@outlook.pt>
2025-01-15 12:04:24 +0000

    update utils

--- packages/design-system/lib/utils.ts
+++ packages/design-system/lib/utils.ts
@@ -1,7 +1,6 @@
 import { clsx } from 'clsx';
-import type { ClassValue } from 'clsx';
-import { toast } from 'sonner';
 import { twMerge } from 'tailwind-merge';
+import type { ClassValue } from 'clsx';
 
 export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs));
 
