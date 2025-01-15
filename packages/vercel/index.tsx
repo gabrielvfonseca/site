66cff0d44b6e9378bd9ffc5b852bde15c89ad6a4
Gabriel Fonseca <jg.fonseca@outlook.pt>
2025-01-15 16:05:27 +0000

    update vercel index analytics

--- packages/vercel/index.tsx
+++ packages/vercel/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
-import { Analytics } from '@/analytics';
-import { SpeedInsights } from '@/speed-insights';
+import { Analytics } from './analytics';
+import { SpeedInsights } from './speed-insights';
 
 export const VercelAnalytics = ({ 
     children 
