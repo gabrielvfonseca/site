39034c61c33aed9822ff1b900746fe22e596cf5a
Gabriel Fonseca <jg.fonseca@outlook.pt>
2025-05-01 22:52:46 +0100

    refactor(design-system): restructure styles and update components

--- packages/design-system/index.tsx
+++ packages/design-system/index.tsx
@@ -1,5 +1,4 @@
 import { AnalyticsProvider } from '@repo/analytics';
-import { AuthProvider } from '@repo/auth/provider';
 import type { ThemeProviderProps } from 'next-themes';
 import { Toaster } from './components/ui/sonner';
 import { TooltipProvider } from './components/ui/tooltip';
@@ -19,11 +18,9 @@ export const DesignSystemProvider = ({
   ...properties
 }: DesignSystemProviderProperties) => (
   <ThemeProvider {...properties}>
-    <AuthProvider privacyUrl={privacyUrl} termsUrl={termsUrl} helpUrl={helpUrl}>
-      <AnalyticsProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-        <Toaster />
-      </AnalyticsProvider>
-    </AuthProvider>
+    <AnalyticsProvider>
+      <TooltipProvider>{children}</TooltipProvider>
+      <Toaster />
+    </AnalyticsProvider>
   </ThemeProvider>
 );
--- packages/design-system/lib/fonts.ts
+++ packages/design-system/lib/fonts.ts
@@ -1,9 +1,13 @@
 import { cn } from '@repo/design-system/lib/utils';
-import { GeistMono } from 'geist/font/mono';
-import { GeistSans } from 'geist/font/sans';
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter-sans',
+});
 
 export const fonts = cn(
-  GeistSans.variable,
-  GeistMono.variable,
+  inter.variable,
   'touch-manipulation font-sans antialiased'
 );
