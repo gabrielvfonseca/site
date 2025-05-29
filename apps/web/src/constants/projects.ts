c2f6cb499dca9ca7b76d56599ca081106ee6348a
Gabriel Fonseca <jg.fonseca@outlook.pt>
2025-05-29 22:51:52 +0100

    feat(web): add MDX components and content constants
    Add MDX components for content rendering and separate content constants for posts and projects

---
+++ apps/web/src/constants/projects.ts
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    href: 'https://github.com/gabriel-fonseca/bad-code-lessons',
+    title: 'Bad Code Lessons',
+    description: 'A collection of lessons learned from writing bad code.',
+  },
+];
+
+export type Projects = typeof projects;
